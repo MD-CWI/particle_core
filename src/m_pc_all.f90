7200aaefbaa356fcb6e3db9c32cd789c1a82b627
Casper Rutjes <casper@rutjes.pro>
2017-04-14 12:49:15 +0200

    added m_units_constants to m_pc_all

--- src/m_pc_all.f90
+++ src/m_pc_all.f90
@@ -1,6 +1,7 @@
 !> Module which contains all particle_core modules, so that a user does not have to
 !> include them separately.
 module m_pc_all
+  use m_units_constants
   use m_cross_sec
   use m_gas
   use m_particle_core
